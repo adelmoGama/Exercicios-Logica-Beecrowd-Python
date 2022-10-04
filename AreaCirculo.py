44bf525c6f9e942d0ea8fbf43dd6f6ce075e3b63
Adelmo Gama <gama.a.jose@gmail.com>
2022-10-04 14:34:40 -0300

    1002 - Iniciante

---
+++ AreaCirculo.py
@@ -0,0 +1,8 @@
+# CONSTANTES/DADOS:
+n: float = 3.14159
+# ENTRADAS:
+raio: float = float(input("Informe o raio: "))
+# PROCESSAMENTO:
+area: float = n * (raio ** 2)
+# SAÍDA:
+print("A=%.4f" % area)
