22027c95d92e22ac7ff14ac715cf3e0dcea0a78b
Adelmo Gama <gama.a.jose@gmail.com>
2022-10-04 14:37:56 -0300

    1007 - Iniciante

---
+++ Diferenca.py
@@ -0,0 +1,9 @@
+# ENTRADA:
+A: int = int(input())
+B: int = int(input())
+C: int = int(input())
+D: int = int(input())
+# PROCESSAMENTO:
+DIFERENCA = int(A*B - C*D)
+# SAÍDA:
+print(f"DIFERENCA = {DIFERENCA}")
