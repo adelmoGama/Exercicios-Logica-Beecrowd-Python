b724504429fdd2cacdaaba64b747600d0aa13ba3
Adelmo Gama <gama.a.jose@gmail.com>
2022-10-06 16:37:16 -0300

    1097 - Iniciante

---
+++ Sequencia3.py
@@ -0,0 +1,25 @@
+i = 1
+j = 7
+for k in range(3):
+    print(f"I={i} J={j}")
+    j -= 1
+i = 3
+j = 9
+for h in range(3):
+    print(f"I={i} J={j}")
+    j -= 1
+i = 5
+j = 11
+for h in range(3):
+    print(f"I={i} J={j}")
+    j -= 1
+i = 7
+j = 13
+for h in range(3):
+    print(f"I={i} J={j}")
+    j -= 1
+i = 9
+j = 15
+for h in range(3):
+    print(f"I={i} J={j}")
+    j -= 1
