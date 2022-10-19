1b961a36bd5e88b30d15d1d4f09627427a31a09e
Adelmo Gama <gama.a.jose@gmail.com>
2022-10-19 15:23:03 -0300

    Iniciante - 1960

---
+++ NumeracaoRomana.py
@@ -0,0 +1,118 @@
+
+N = int(input())
+N = list(str(N))
+if int(len(N)) == 1:
+    if int(N[0]) == 9:
+        print("IX")
+    elif int(N[0]) == 8:
+        print("VIII")
+    elif int(N[0]) == 7:
+        print("VII")
+    elif int(N[0]) == 6:
+        print("VI")
+    elif int(N[0]) == 5:
+        print("V")
+    elif int(N[0]) == 4:
+        print("IV")
+    elif int(N[0]) == 3:
+        print("III")
+    elif int(N[0]) == 2:
+        print("II")
+    elif int(N[0]) == 1:
+        print("I")
+elif int(len(N)) == 2:
+    if int(N[0]) == 9:
+        print("XC", end="")
+    elif int(N[0]) == 8:
+        print("LXXX", end="")
+    elif int(N[0]) == 7:
+        print("LXX", end="")
+    elif int(N[0]) == 6:
+        print("LX", end="")
+    elif int(N[0]) == 5:
+        print("L", end="")
+    elif int(N[0]) == 4:
+        print("XL", end="")
+    elif int(N[0]) == 3:
+        print("XXX", end="")
+    elif int(N[0]) == 2:
+        print("XX", end="")
+    elif int(N[0]) == 1:
+        print("X", end="")
+    if int(N[1]) == 9:
+        print("IX")
+    elif int(N[1]) == 8:
+        print("VIII")
+    elif int(N[1]) == 7:
+        print("VII")
+    elif int(N[1]) == 6:
+        print("VI")
+    elif int(N[1]) == 5:
+        print("V")
+    elif int(N[1]) == 4:
+        print("IV")
+    elif int(N[1]) == 3:
+        print("III")
+    elif int(N[1]) == 2:
+        print("II")
+    elif int(N[1]) == 1:
+        print("I")
+    elif int(N[1]) == 0:
+        print("")
+elif int(len(N)) == 3:
+    if int(N[0]) == 9:
+        print("CM", end="")
+    elif int(N[0]) == 8:
+        print("DCCC", end="")
+    elif int(N[0]) == 7:
+        print("DCC", end="")
+    elif int(N[0]) == 6:
+        print("DC", end="")
+    elif int(N[0]) == 5:
+        print("D", end="")
+    elif int(N[0]) == 4:
+        print("CD", end="")
+    elif int(N[0]) == 3:
+        print("CCC", end="")
+    elif int(N[0]) == 2:
+        print("CC", end="")
+    elif int(N[0]) == 1:
+        print("C", end="")
+    if int(N[1]) == 9:
+        print("XC", end="")
+    elif int(N[1]) == 8:
+        print("LXXX", end="")
+    elif int(N[1]) == 7:
+        print("LXX", end="")
+    elif int(N[1]) == 6:
+        print("LX", end="")
+    elif int(N[1]) == 5:
+        print("L", end="")
+    elif int(N[1]) == 4:
+        print("XL", end="")
+    elif int(N[1]) == 3:
+        print("XXX", end="")
+    elif int(N[1]) == 2:
+        print("XX", end="")
+    elif int(N[1]) == 1:
+        print("X", end="")
+    if int(N[2]) == 9:
+        print("IX")
+    elif int(N[2]) == 8:
+        print("VIII")
+    elif int(N[2]) == 7:
+        print("VII")
+    elif int(N[2]) == 6:
+        print("VI")
+    elif int(N[2]) == 5:
+        print("V")
+    elif int(N[2]) == 4:
+        print("IV")
+    elif int(N[2]) == 3:
+        print("III")
+    elif int(N[2]) == 2:
+        print("II")
+    elif int(N[2]) == 1:
+        print("I")
+    elif int(N[2]) == 0:
+        print("")
