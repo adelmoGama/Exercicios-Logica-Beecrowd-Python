1168dbc5b8b080452efe81a31bdc62930c62924f
Adelmo Gama <gama.a.jose@gmail.com>
2022-10-22 10:58:59 -0300

    Iniciante - 2582

---
+++ system_of_a_download.py
@@ -0,0 +1,27 @@
+C = int(input())
+soma = 0
+for i in range(C):
+    X, Y = input().split()
+    soma = int(X) + int(Y)
+    if soma == 0:
+        print("PROXYCITY")
+    elif soma == 1:
+        print("P.Y.N.G.")
+    elif soma == 2:
+        print("DNSUEY!")
+    elif soma == 3:
+        print("SERVERS")
+    elif soma == 4:
+        print("HOST!")
+    elif soma == 5:
+        print("CRIPTONIZE")
+    elif soma == 6:
+        print("OFFLINE DAY")
+    elif soma == 7:
+        print("SALT")
+    elif soma == 8:
+        print("ANSWER!")
+    elif soma == 9:
+        print("RAR?")
+    elif soma == 10:
+        print("WIFI ANTENNAS")
